df92aff939e64d5028ef0bdb3c97db145a1e8cef
munazza-sarosh <saroshmunazza@gmail.com>
2026-09-12 02:22:16 +0500

    add animation in cards

--- script.js
+++ script.js
@@ -5,3 +5,86 @@ function toggleMenu() {
     menu.classList.toggle("active");
 
 }
+const cards = document.querySelectorAll(".project-card");
+
+window.addEventListener("scroll", () => {
+    cards.forEach(card => {
+        const position = card.getBoundingClientRect().top;
+
+        if (position < window.innerHeight - 100) {
+            card.classList.add("show");
+        }
+    });
+});
+
+// const card = document.querySelectorAll(".about-container");
+
+// window.addEventListener("scroll", () => {
+//     cards.forEach(card => {
+//         const position = card.getBoundingClientRect().top;
+
+//         if (position < window.innerHeight - 100) {
+//             card.classList.add("show");
+//         }
+//     });
+// });
+
+const cards_ = document.querySelectorAll(".hero");
+
+window.addEventListener("scroll", () => {
+    cards_.forEach(card => {
+        const position = card.getBoundingClientRect().top;
+
+        if (position < window.innerHeight - 100) {
+            card.classList.add("show");
+        }
+    });
+})
+
+const cards1 = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+    cards1.forEach(card => {
+        const position = card.getBoundingClientRect().top;
+
+        if (position < window.innerHeight - 100) {
+            card.classList.add("show");
+        }
+    });
+})
+
+const cards2 = document.querySelectorAll(".skills-section");
+
+window.addEventListener("scroll", () => {
+    cards2.forEach(card => {
+        const position = card.getBoundingClientRect().top;
+
+        if (position < window.innerHeight - 100) {
+            card.classList.add("show");
+        }
+    });
+})
+
+const cards3 = document.querySelectorAll(".education-section");
+
+window.addEventListener("scroll", () => {
+    cards3.forEach(card => {
+        const position = card.getBoundingClientRect().top;
+
+        if (position < window.innerHeight - 100) {
+            card.classList.add("show");
+        }
+    });
+})
+
+const cards4 = document.querySelectorAll(".contact-section");
+
+window.addEventListener("scroll", () => {
+    cards4.forEach(card => {
+        const position = card.getBoundingClientRect().top;
+
+        if (position < window.innerHeight - 100) {
+            card.classList.add("show");
+        }
+    });
+})
